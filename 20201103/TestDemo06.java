534b1ab6c7de66d0646b9de7ec3390b20d95b63b
xwq <1561493487@qq.com>
2020-11-03 21:37:55 +0800

    队列和栈的练习

---
+++ 20201103/TestDemo06.java
@@ -0,0 +1,12 @@
+package test20201103;
+//栈的压入、弹出序列   牛客     https://www.nowcoder.com/practice/d77d11405cc7470d82554cb392585106?tpId=13&&tqId=11174&rp=1&ru=/activity/oj&qru=/ta/coding-interviews/question-ranking
+import java.util.ArrayList;
+
+public class Solution {
+    public boolean IsPopOrder(int [] pushA,int [] popA) {
+        return true;
+    }
+}
+
+public class TestDemo06 {
+}
